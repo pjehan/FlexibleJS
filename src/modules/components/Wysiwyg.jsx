fca57ad2ebb52dfac8ef5b9be9f545b11bcf2fca
pjehan <pierre.jehan@gmail.com>
2017-02-20 21:42:17 +0100

    Fix issue with WYSIWYG within bloc

--- src/modules/components/Wysiwyg.jsx
+++ src/modules/components/Wysiwyg.jsx
@@ -2,22 +2,24 @@ import React from 'react'
 
 import { FormControl } from 'react-bootstrap'
 
+import randomstring from 'randomstring';
+
 module.exports =  React.createClass({
-  
+
   getInitialState: function() {
-    return {id: this.props.template.id, value: this.props.value};
+    return {id: this.props.template.id, value: this.props.value, elementClass: randomstring.generate({length: 8, charset: 'alphabetic'})};
   },
-  
+
   componentDidMount: function() {
     var self = this;
-    
+
     this.setState({id: this.props.template.id, value: this.props.value});
-    
-    var textarea = $('textarea[name=' + this.props.template.id + ']');
-    
+
+    var textarea = $('textarea.' + this.state.elementClass);
+
     textarea.summernote({
       lang: 'fr-FR',
-      height: 350,
+      height: 250,
       dialogsInBody: true,
       toolbar: [
         ['style', ['style']],
@@ -29,44 +31,46 @@ module.exports =  React.createClass({
         ['view', ['fullscreen', 'codeview']]
       ]
     });
-    
+
     textarea.on('summernote.keyup', function() {
+      console.log(textarea.summernote('code'));
       self.setState({value: textarea.summernote('code')}, function() {
         self.props.handleChange(self.state);
       });
     });
   },
-  
+
   componentWillReceiveProps: function(newProps) {
     this.setState({id: newProps.template.id, value: newProps.value});
   },
-  
+
   handleChange: function(value) {
     this.setState({value: value}, function() {
       this.props.handleChange(this.state);
     });
   },
-  
+
   render() {
-    
+
     var attrs = {};
     for (var prop in this.props.template) {
-      if (this.props.template.hasOwnProperty(prop) && !['id', 'title', 'help', 'header'].includes(prop)) {
+      if (this.props.template.hasOwnProperty(prop) && !['id', 'title', 'type', 'help', 'header'].includes(prop)) {
         attrs[prop] = this.props.template[prop];
       }
     }
-    
+
     return (
       <FormControl
         componentClass="textarea"
         {...attrs}
+        bsClass={this.state.elementClass}
         name={this.props.template.id}
         value={(this.state.value) ? this.state.value : ''}
         placeholder={this.props.template.placeholder}
         onChange={this.handleChange}
         />
     );
-    
+
   }
-  
+
 })
