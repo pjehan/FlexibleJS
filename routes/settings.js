61769b420fc6732e4affd68da6693ad222cc4cd6
pjehan <pierre.jehan@gmail.com>
2017-07-18 15:57:48 +0200

    Import / Export upload dir

--- routes/settings.js
+++ routes/settings.js
@@ -3,12 +3,14 @@ var router = express.Router();
 var multiparty = require('multiparty');
 var path = require('path');
 var fs = require('fs');
+var archiver = require('archiver');
+var unzip =  require('unzip-stream');
 var moment = require("moment");
 var spawn = require('child_process').spawn;
 
 const backupDir = path.join(__dirname, '../public/backup/');
 
-function createBackup(dbname, callback) {
+function createDbBackup(dbname, callback) {
   const filename = dbname + '_db_' + moment().format('YYYYMMDDHHmmss') + '.gz';
   const backupPath = backupDir + filename;
 
@@ -26,6 +28,31 @@ function createBackup(dbname, callback) {
   });
 }
 
+function createFilesBackup(dbname, callback) {
+  return new Promise((resolve, reject) => {
+    const filename = dbname + '_files_' + moment().format('YYYYMMDDHHmmss') + '.zip';
+    var archive = archiver('zip');
+    var output = fs.createWriteStream(backupDir + filename);
+
+    // Create backup dir if not exists
+    if (!fs.existsSync(backupDir)){
+      fs.mkdirSync(backupDir);
+    }
+
+    output.on('close', function() {
+      resolve('/backup/' + filename);
+    });
+
+    archive.on('error', function(err) {
+      reject(err);
+    });
+
+    archive.pipe(output);
+    archive.directory(path.join(__dirname, '../public/uploads/'), false);
+    archive.finalize();
+  })
+}
+
 router.get('/export-database', function(req, res) {
   const dbname = req.app.locals.config.db.name;
   createBackup(dbname, function(filepath) {
@@ -38,11 +65,10 @@ router.post('/import-database/:name', function(req, res) {
   const fieldName = req.params.name;
   const form = new multiparty.Form();
 
-  // Upload images
   form.parse(req, function(err, fields, files) {
     var file = files[fieldName][0];
 
-    createBackup(dbname, function(filepath) {
+    createDbBackup(dbname, function(filepath) {
       spawn('mongorestore', [
         '--db', dbname,
         '--gzip',
@@ -56,5 +82,40 @@ router.post('/import-database/:name', function(req, res) {
     });
   });
 });
+
+router.get('/export-files', (req, res) => {
+  const dbname = req.app.locals.config.db.name;
+  createFilesBackup(dbname)
+  .then((filepath) => {
+    res.json({path: filepath});
+  })
+  .catch((err) => {
+    console.log(err);
+    res.statut(500).json(err);
+  })
+})
+
+router.post('/import-files/:name', (req, res) => {
+  const dbname = req.app.locals.config.db.name;
+  const fieldName = req.params.name;
+  const form = new multiparty.Form();
+
+  form.parse(req, function(err, fields, files) {
+    if (err) throw err;
+
+    // Create files backup before import
+    createFilesBackup(dbname)
+    .then((filepath) => {
+      var file = files[fieldName][0];
+      fs.createReadStream(file.path).pipe(unzip.Extract({ path: path.join(__dirname, '../public/uploads/') }));
+      res.json({success: true});
+    })
+    .catch((err) => {
+      console.log(err);
+      res.statut(500).json(err);
+    })
+
+  });
+})
 
 module.exports = router;
--- src/modules/Settings.jsx
+++ src/modules/Settings.jsx
@@ -4,16 +4,16 @@ import { browserHistory } from 'react-router'
 
 import { Grid, Panel, Button, ButtonToolbar, ControlLabel, FormControl } from 'react-bootstrap'
 
-var Settings = React.createClass({
+class Settings extends React.Component {
 
-  exportDatabase: function(event) {
+  exportDatabase(event) {
     $.get('/api/settings/export-database')
     .done(function(result){
       window.location.href = result.path;
     });
-  },
+  }
 
-  importDatabase: function(event) {
+  importDatabase(event) {
     var form = $(event.target).parents('form')[0];
 
     var data = new FormData(form);
@@ -28,12 +28,62 @@ var Settings = React.createClass({
     })
     .done(function(data) {
       console.log(data);
+      this.props.handleNotification({
+        title: this.props.intl.formatMessage({id: 'notification.importdb.success.title'}),
+        message: this.props.intl.formatMessage({id: 'notification.importdb.success.message'})
+      });
     })
     .fail(function(jqXHR, textStatus) {
       console.log(jqXHR);
       console.log(textStatus);
+      this.props.handleNotification({
+        title: this.props.intl.formatMessage({id: 'notification.importdb.error.title'}),
+        message: this.props.intl.formatMessage({id: 'notification.importdb.error.message'})
+      });
     });
-  },
+  }
+
+  exportFiles() {
+    fetch('/api/settings/export-files')
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      window.location.href = data.path;
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+  }
+
+  importFiles(event) {
+    var form = $(event.target).parents('form')[0];
+    var data = new FormData(form);
+
+    fetch('/api/settings/import-files/' + event.target.name, {
+      method: 'POST',
+      body: data,
+      cache: false,
+      contentType: false,
+      processData: false,
+    })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      this.props.handleNotification({
+        title: this.props.intl.formatMessage({id: 'notification.importfiles.success.title'}),
+        message: this.props.intl.formatMessage({id: 'notification.importfiles.success.message'})
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+      this.props.handleNotification({
+        title: this.props.intl.formatMessage({id: 'notification.importfiles.error.title'}),
+        message: this.props.intl.formatMessage({id: 'notification.importfiles.error.message'})
+      });
+    })
+  }
 
   render() {
 
@@ -58,16 +108,18 @@ var Settings = React.createClass({
 
           <h4><FormattedMessage id="title.files"/></h4>
           <ButtonToolbar>
-            <ControlLabel className="btn btn-primary" htmlFor="formImportFiles">
-              <i className="fa fa-download"></i><FormControl id="formImportFiles" type="file" className="hidden"/> <FormattedMessage id="btn.import"/>
-            </ControlLabel>
-            <Button bsStyle="primary"><i className="fa fa-upload"></i> <FormattedMessage id="btn.export"/></Button>
+            <form encType="multipart/form-data">
+              <ControlLabel className="btn btn-primary" htmlFor="formImportFiles">
+                <i className="fa fa-download"></i><FormControl id="formImportFiles" type="file" name="formImportFiles" className="hidden" onChange={this.importFiles.bind(this)}/> <FormattedMessage id="btn.import"/>
+              </ControlLabel>
+            </form>
+            <Button bsStyle="primary" onClick={this.exportFiles}><i className="fa fa-upload"></i> <FormattedMessage id="btn.export"/></Button>
           </ButtonToolbar>
 
         </Panel>
       </Grid>
     )
   }
-});
+}
 
-module.exports = injectIntl(Settings);
+module.exports = injectIntl(Settings)
