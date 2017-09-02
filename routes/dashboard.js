7fe490f415a65d49c86b7f37fda76e904a676f07
pjehan <pierre.jehan@gmail.com>
2017-09-02 13:26:10 +0200

    Fix dashboard crash when gapi_key.json is missing

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,26 +1,29 @@
 var express = require('express')
 var router = express.Router()
 var path = require('path')
+var fs = require('fs')
 var google = require('googleapis')
 
 router.get('/gapi-key/:viewId/:email', function(req, res) {
   const serviceAccountKeyFile = path.join(__dirname, '/../gapi_key.json')
-  const serviceAccountEmail = (req.params.email !== 'undefined') ? req.params.email : req.app.locals.config.gapi.email
-  const viewId = (req.params.viewId !== 'undefined') ? req.params.viewId : req.app.locals.config.gapi.viewId
 
-  var jwtClient = new google.auth.JWT(
-    serviceAccountEmail,
-    serviceAccountKeyFile,
-    null,
-    ['https://www.googleapis.com/auth/analytics.readonly']
-  )
+  fs.stat(serviceAccountKeyFile, (err, stats) => {
+    if (err) res.status(500).json(err)
 
-  jwtClient.authorize(function(err, token) {
-    if (err) {
-      console.log(err)
-      return
-    }
-    return res.json({token: token, viewId: viewId})
+    const serviceAccountEmail = (req.params.email !== 'undefined') ? req.params.email : req.app.locals.config.gapi.email
+    const viewId = (req.params.viewId !== 'undefined') ? req.params.viewId : req.app.locals.config.gapi.viewId
+
+    var jwtClient = new google.auth.JWT(
+      serviceAccountEmail,
+      serviceAccountKeyFile,
+      null,
+      ['https://www.googleapis.com/auth/analytics.readonly']
+    )
+
+    jwtClient.authorize(function(err, token) {
+      if (err) res.status(500).json(err)
+      return res.json({token: token, viewId: viewId})
+    })
   })
 })
 
