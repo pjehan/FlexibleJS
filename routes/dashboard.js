359625b09f175ee5cd5fdf9df4d5d6caeb283b13
pjehan <pierre.jehan@gmail.com>
2018-01-05 19:45:05 +0100

    Fix gapi missing error 500

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -8,20 +8,30 @@ router.get('/gapi-key/:viewId/:email', function(req, res) {
   const serviceAccountKeyFile = path.join(__dirname, '/../gapi_key.json')
 
   fs.stat(serviceAccountKeyFile, (err, stats) => {
-    if (err) res.status(500).json(err)
+    if (err) return res.status(500).json(err)
 
-    const serviceAccountEmail = (req.params.email !== 'undefined') ? req.params.email : req.app.locals.config.gapi.email
-    const viewId = (req.params.viewId !== 'undefined') ? req.params.viewId : req.app.locals.config.gapi.viewId
+    var serviceAccountEmail = req.params.email
+    var viewId = req.params.viewId
 
-    var jwtClient = new google.auth.JWT(
-      serviceAccountEmail,
-      serviceAccountKeyFile,
-      null,
-      ['https://www.googleapis.com/auth/analytics.readonly']
-    )
+    if (typeof req.app.locals.config.gapi !== 'undefined') {
+      serviceAccountEmail = req.app.locals.config.gapi.email
+      viewId = req.app.locals.config.gapi.viewId
+    }
+
+    try {
+      var jwtClient = new google.auth.JWT(
+        serviceAccountEmail,
+        serviceAccountKeyFile,
+        null,
+        ['https://www.googleapis.com/auth/analytics.readonly']
+      )
+    } catch (err) {
+      return res.status(500).json(err)
+    }
+
 
     jwtClient.authorize(function(err, token) {
-      if (err) res.status(500).json(err)
+      if (err) return res.status(500).json(err)
       return res.json({token: token, viewId: viewId})
     })
   })
